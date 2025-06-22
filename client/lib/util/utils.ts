aa34eefd4de6bce0d2f958afda2e4e7cfba11b64
Jared Tucker <jtuc0006@student.monash.edu>
2025-06-22 15:29:20 +1000

    Setup base components

---
+++ client/lib/util/utils.ts
@@ -0,0 +1,52 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function undefinedIfNull<T>(value: T | null): T | undefined {
+    return value === null ? undefined : value;
+}
+
+export const getInitials = (name: string): string => {
+    if (!name.trim()) {
+        return "";
+    }
+
+    // Split the name into parts, filtering out empty strings caused by extra spaces
+    const nameParts = name.trim().split(/\s+/);
+
+    // Extract the first letter of each part and limit to the first two
+    const initials = nameParts.map((part) => part[0].toUpperCase()).slice(0, 2);
+
+    // Join the initials into a single string
+    return initials.join("");
+};
+
+export const toTitleCase = (value: string | null | undefined): string => {
+    if (!value) return "";
+
+    return value
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(" ");
+};
+
+export const isUUID = (value: string): boolean => {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(value);
+};
+
+export const allNotNull = <T>(values: (T | null)[]): values is NonNullable<T>[] => {
+    return values.every((value) => value !== null);
+};
+
+export const api = () => {
+    const url = process.env.NEXT_PUBLIC_API_URL;
+    if (!url) {
+        throw new Error("API URL not configured");
+    }
+
+    return url;
+};
