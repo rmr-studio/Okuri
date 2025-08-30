ce668d40f0487e1c1a0e7ac4abdf403762fb484d
Jared Tucker <jntucker02@gmail.com>
2025-08-30 10:18:09 +1000

    fix id param

--- client/hooks/useOrganisation.tsx
+++ client/hooks/useOrganisation.tsx
@@ -6,14 +6,14 @@ import { useParams } from "next/navigation";
 export const useOrganisation = () => {
     const { session, loading } = useAuth();
     // Extract organization name from URL params
-    // Assuming the route is defined like: /dashboard/organisation/:orgId
-    const { id: orgId } = useParams<{ id: string }>();
+    // Assuming the route is defined like: /dashboard/organisation/:organisationId
+    const { organisationId } = useParams<{ organisationId: string }>();
 
     // Use TanStack Query to fetch organization data
     const query = useQuery({
-        queryKey: ["organization", orgId], // Unique key for caching
-        queryFn: () => getOrganisation(session, { organisationId: orgId }), // Fetch function
-        enabled: !!orgId && !!session?.user.id, // Only fetch if orgName exists and the user is authenticated
+        queryKey: ["organisation", organisationId], // Unique key for caching
+        queryFn: () => getOrganisation(session, { organisationId }), // Fetch function
+        enabled: !!organisationId && !!session?.user.id, // Only fetch if orgName exists and the user is authenticated
         retry: 1,
         staleTime: 5 * 60 * 1000, // Cache for 5 minutes
     });
