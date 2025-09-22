74951150c66fb9c9a9ce422d8f1d6e0c7f72cc06
Jared Tucker <jntucker02@gmail.com>
2025-09-23 04:34:23 +1000

    Reusable Audit Entity extension

--- core/src/main/kotlin/okuri/core/entity/util/AuditableEntity.kt
+++ core/src/main/kotlin/okuri/core/entity/util/AuditableEntity.kt
@@ -1,4 +1,33 @@
 package okuri.core.entity.util
 
-class AuditableEntity {
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.ZonedDateTime
+import java.util.*
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class AuditableEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: ZonedDateTime? = null
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    var updatedAt: ZonedDateTime? = null
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, columnDefinition = "uuid")
+    var createdBy: UUID? = null
+
+    @LastModifiedBy
+    @Column(name = "updated_by", columnDefinition = "uuid")
+    var updatedBy: UUID? = null
 }
